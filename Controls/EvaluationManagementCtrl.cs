78219c0159871ddea328acbbc3d3eee6adbd33ad
cntimothy <cntimothy1990@gmail.com>
2013-07-07 19:26:16 +0800

    ENH:add controller for EvaluationManagement

---
+++ Controls/EvaluationManagementCtrl.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using DataStructure;
+
+namespace Controls
+{
+    public class EvaluationManagementCtrl
+    {
+        /// <summary>
+        /// 根据部门查询被考评人信息（ID, Date, Name, Sex, Department, Job, IDNo, Birthday, Telephone, Email, Fund, Character, Company, StartTime, StopTime, Summary（考评完成/考评总数））。table不为空返回true，否则返回false
+        /// </summary>
+        /// <param name="table">考评人信息</param>
+        /// <param name="depart">部门</param>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public static bool GetAllByDepart(ref DataTable table, string depart, ref string exception)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 开始考评。设置成功返回true，否则返回false
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public static bool StartEvaluation(ref string exception)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 结束指定被考评人的考评，设置成功返回true，否则返回false
+        /// </summary>
+        /// <param name="evaluated">被考评人ID</param>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public static bool StopEvaluation(string evaluated, ref string exception)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 根据被考评人ID查询考评表，查询成功（存在考核表）返回true，否则返回false；
+        /// </summary>
+        /// <param name="ID">被考评人ID</param>
+        /// <param name="evaluateTable">考核表</param>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public static bool GetEvaluateTable(string ID, ref EvaluateTable evaluateTable, ref string exception)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 更新指定被考评人的分数，更新成功返回true，否则返回false
+        /// </summary>
+        /// <param name="ID">被考评人ID</param>
+        /// <param name="scores">包含分数的数组</param>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public static bool UpdateScore(string ID, string[] scores, ref string exception)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 获取所有考评人信息（ID, Name, Sex, Company, Telephone, Relation），table不为空，返回true，否则返回false
+        /// </summary>
+        /// <param name="table">考评人信息</param>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public static bool GetEvaluator(ref DataTable table, ref string exception)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 根据指定部门获取所有考评人信息（ID, Name, Sex, Company, Telephone, Relation），table不为空，返回true，否则返回false
+        /// </summary>
+        /// <param name="table">被考评人信息</param>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public static bool GetEvaluatorByDepart(ref DataTable table, ref string exception)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 给所有考评人发送短信，发送成功返回true，否则返回false
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public static bool SendMessageToAll(ref string exception)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 分部门给考评人发送短信，发送成功返回true，否则返回false
+        /// </summary>
+        /// <param name="depart"></param>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public static bool SendMessageByDepart(string depart, ref string exception)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 给指定的考评人发送短信，发送成功返回true，否则返回false
+        /// </summary>
+        /// <param name="IDs"></param>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public static bool SendMessage(List<string> IDs, ref string exception)
+        {
+            return true;
+        }
+    }
+}
--- DataStructure/EvaluateTable.cs
+++ DataStructure/EvaluateTable.cs
@@ -5,96 +5,257 @@ using System.Text;
 
 namespace DataStructure
 {
-    public class EvaluateTable
+    public class Quota
     {
         #region Private Field
-        string postName;                                    //岗位名称（岗位责任书）
-        string laborDep;                                    //用工部门（岗位责任书）
-        string laborUnit;                                   //用工单位（岗位责任书）
-        string startTime;                                   //考核开始时间（被考评人信息）
-        string stopTime;                                    //考核结束时间（被考评人信息）
-        List<Dictionary<string, string>> keyDuty;           //关键岗位职责指标
-        List<Dictionary<string, string[]>> keyQualify;      //关键岗位胜任能力指标
-        List<Dictionary<string, string[]>> keyAttribute;    //关键工作态度指标
-        List<Dictionary<string, string>> duty;              //岗位职责指标
-        List<Dictionary<string, string[]>> qualify;         //岗位胜任能力指标
-        List<Dictionary<string, string[]>> attribute;       //岗位工作态度指标
-        List<Dictionary<string, string>> reject;            //否决指标
+        string title;
+        string[] content;
+        string score;
         #endregion
 
         #region Public Field
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
 
+        public string[] Content
+        {
+            get { return content; }
+            set { content = value; }
+        }
+
+        public string Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// 如果content的数量不为1或者4，抛出Exception
+        /// </summary>
+        /// <param name="title">标题</param>
+        /// <param name="content">指标内容</param>
+        /// <param name="score">分数</param>
+        public Quota(string title, string[] content, string score)
+        {
+            if (content.Length == 1)
+            {
+                this.title = title;
+                this.content = new string[1];
+                this.content[0] = content[0];
+                this.score = score;
+            }
+            else if (content.Length == 4)
+            {
+                this.title = title;
+                this.content = new string[4];
+                for (int i = 0; i < 4; i++)
+                {
+                    this.content[i] = content[i];
+                }
+                this.score = score;
+            }
+            else 
+            {
+                Exception e = new Exception("指标数目错误");
+                throw e;
+            }
+        }
+        #endregion
+
+    }
+
+    public class EvaluateTable
+    {
+        #region Private Field
+        string evaluatedName;
+        string postName;                //岗位名称（岗位责任书）
+        string laborDep;                //用工部门（岗位责任书）
+        string laborUnit;               //用工单位（岗位责任书）
+        string startTime;               //考评开始时间（被考评人信息）
+        string stopTime;                //考评结束事件（被考评人信息）
+        List<Quota> keyResponse;
+        List<Quota> keyQualify;
+        List<Quota> keyAttitude;
+        List<Quota> response;
+        List<Quota> qualify;
+        List<Quota> attitude;
+        List<Quota> regect;
+        string keyWeight;
+        string responseWeight;
+        string qualifyWeitht;
+        string attitudeWeight;
+        string rejectWeight;        
+        #endregion
+
+        #region Public Field
+        /// <summary>
+        /// 配考评人姓名
+        /// </summary>
+        public string EvaluatedName
+        {
+            get { return evaluatedName; }
+            set { evaluatedName = value; }
+        }
+
+        /// <summary>
+        /// 岗位名称（岗位责任书）
+        /// </summary>
         public string PostName
         {
             get { return postName; }
             set { postName = value; }
         }
 
+        /// <summary>
+        /// 用工部门（岗位责任书）
+        /// </summary>
         public string LaborDep
         {
             get { return laborDep; }
             set { laborDep = value; }
         }
 
+        /// <summary>
+        /// 用工单位（岗位责任书）
+        /// </summary>
         public string LaborUnit
         {
             get { return laborUnit; }
             set { laborUnit = value; }
         }
 
+        /// <summary>
+        /// 考评开始时间（被考评人信息）
+        /// </summary>
         public string StartTime
         {
             get { return startTime; }
             set { startTime = value; }
         }
 
+        /// <summary>
+        /// 考评结束时间（被考评人信息）
+        /// </summary>
         public string StopTime
         {
             get { return stopTime; }
             set { stopTime = value; }
         }
 
-        public List<Dictionary<string, string>> KeyDuty
+        /// <summary>
+        /// 关键岗位职责指标
+        /// </summary>
+        public List<Quota> KeyResponse
         {
-            get { return keyDuty; }
-            set { keyDuty = value; }
+            get { return keyResponse; }
+            set { keyResponse = value; }
         }
 
-        private List<Dictionary<string, string[]>> KeyQualify
+        /// <summary>
+        /// 关键岗位胜任能力指标
+        /// </summary>
+        public List<Quota> KeyQualify
         {
             get { return keyQualify; }
             set { keyQualify = value; }
         }
 
-        private List<Dictionary<string, string[]>> KeyAttribute
+        /// <summary>
+        /// 关键工作态度指标
+        /// </summary>
+        public List<Quota> KeyAttitude
         {
-            get { return keyAttribute; }
-            set { keyAttribute = value; }
+            get { return keyAttitude; }
+            set { keyAttitude = value; }
         }
 
-        public List<Dictionary<string, string>> Duty
+        /// <summary>
+        /// 岗位职责指标
+        /// </summary>
+        public List<Quota> Response
         {
-            get { return duty; }
-            set { duty = value; }
+            get { return response; }
+            set { response = value; }
         }
 
-        private List<Dictionary<string, string[]>> Qualify
+        /// <summary>
+        /// 岗位胜任能力指标
+        /// </summary>
+        public List<Quota> Qualify
         {
             get { return qualify; }
             set { qualify = value; }
         }
 
-        private List<Dictionary<string, string[]>> Attribute
+        /// <summary>
+        /// 工作态度指标
+        /// </summary>
+        public List<Quota> Attitude
         {
-            get { return attribute; }
-            set { attribute = value; }
+            get { return attitude; }
+            set { attitude = value; }
         }
 
-        public List<Dictionary<string, string>> Reject
+        /// <summary>
+        /// 否决指标
+        /// </summary>
+        public List<Quota> Regect
         {
-            get { return reject; }
-            set { reject = value; }
+            get { return regect; }
+            set { regect = value; }
+        }
+
+        /// <summary>
+        /// 关键绩效指标权重
+        /// </summary>
+        public string KeyWeight
+        {
+            get { return keyWeight; }
+            set { keyWeight = value; }
+        }
+
+        /// <summary>
+        /// 岗位职责指标权重
+        /// </summary>
+        public string ResponseWeight
+        {
+            get { return responseWeight; }
+            set { responseWeight = value; }
+        }
+
+        /// <summary>
+        /// 岗位胜任能力指标权重
+        /// </summary>
+        public string QualifyWeitht
+        {
+            get { return qualifyWeitht; }
+            set { qualifyWeitht = value; }
+        }
+
+        /// <summary>
+        /// 工作态度指标权重
+        /// </summary>
+        public string AttitudeWeight
+        {
+            get { return attitudeWeight; }
+            set { attitudeWeight = value; }
+        }
+
+        /// <summary>
+        /// 否决指标权重
+        /// </summary>
+        public string RejectWeight
+        {
+            get { return rejectWeight; }
+            set { rejectWeight = value; }
         }
         #endregion
+
+
     }
 }
