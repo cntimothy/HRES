1a5c974c9705d95e4df77f19e08454cf6ac0a52f
cntimothy <cntimothy1990@gmail.com>
2013-07-15 16:39:44 +0800

    ENH:add test case for make and check evaluator list

--- Controls/EvaluatorManagementCtrl.cs
+++ Controls/EvaluatorManagementCtrl.cs
@@ -17,6 +17,71 @@ namespace Controls
         /// <returns>dt不为空，返回true，否则返回false</returns>
         public static bool GetAll(ref DataTable dt, ref string exception)
         {
+            dt.Columns.Add("ID");
+            dt.Columns.Add("Date");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Sex");
+            dt.Columns.Add("Depart");
+            dt.Columns.Add("Job");
+            dt.Columns.Add("IDNo");
+            dt.Columns.Add("Birthday");
+            dt.Columns.Add("Fund");
+            dt.Columns.Add("Character");
+            dt.Columns.Add("Company");
+            dt.Columns.Add("StartTime");
+            dt.Columns.Add("StopTime");
+            dt.Columns.Add("Status");
+            dt.Columns.Add("Comment");
+            string[] values1 = new string[] { "1", "2010-3-3", "高1", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values2 = new string[] { "2", "2010-3-3", "高2", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "1300000002", "gao1@tongji.edu.cn", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values3 = new string[] { "3", "2010-3-3", "高3", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values4 = new string[] { "4", "2010-3-3", "高4", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values5 = new string[] { "5", "2010-3-3", "高5", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values6 = new string[] { "6", "2010-3-3", "高6", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values7 = new string[] { "123456", "2010-3-3", "高7", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values8 = new string[] { "123456", "2010-3-3", "高8", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values9 = new string[] { "123456", "2010-3-3", "高9", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values10 = new string[] { "123456", "2010-3-3", "高10", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "test for comment" };
+            string[] values11 = new string[] { "123456", "2010-3-3", "高11", "男", "外国语学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values12 = new string[] { "123456", "2010-3-3", "高12", "男", "外国语学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values13 = new string[] { "123456", "2010-3-3", "高13", "男", "外国语学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values14 = new string[] { "123456", "2010-3-3", "高14", "男", "外国语学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "test for comment" };
+            string[] values15 = new string[] { "123456", "2010-3-3", "高15", "男", "外国语学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values16 = new string[] { "123456", "2010-3-3", "高16", "男", "外国语学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values17 = new string[] { "123456", "2010-3-3", "高17", "男", "外国语学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values18 = new string[] { "123456", "2010-3-3", "高18", "男", "外国语学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values19 = new string[] { "123456", "2010-3-3", "高19", "男", "人事处", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values20 = new string[] { "123456", "2010-3-3", "高20", "男", "人事处", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "2", "" };
+            string[] values21 = new string[] { "123456", "2010-3-3", "高21", "男", "人事处", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "2", "" };
+            string[] values22 = new string[] { "123456", "2010-3-3", "高22", "男", "人事处", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "3", "" };
+            string[] values23 = new string[] { "123456", "2010-3-3", "高23", "男", "人事处", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "3", "" };
+            string[] values24 = new string[] { "123456", "2010-3-3", "高24", "男", "人事处", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "4", "" };
+            string[] values25 = new string[] { "123456", "2010-3-3", "高25", "男", "人事处", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "4", "" };
+            dt.Rows.Add(values1);
+            dt.Rows.Add(values2);
+            dt.Rows.Add(values3);
+            dt.Rows.Add(values4);
+            dt.Rows.Add(values5);
+            dt.Rows.Add(values6);
+            dt.Rows.Add(values7);
+            dt.Rows.Add(values8);
+            dt.Rows.Add(values9);
+            dt.Rows.Add(values10);
+            dt.Rows.Add(values11);
+            dt.Rows.Add(values12);
+            dt.Rows.Add(values13);
+            dt.Rows.Add(values14);
+            dt.Rows.Add(values15);
+            dt.Rows.Add(values16);
+            dt.Rows.Add(values17);
+            dt.Rows.Add(values18);
+            dt.Rows.Add(values19);
+            dt.Rows.Add(values20);
+            dt.Rows.Add(values21);
+            dt.Rows.Add(values22);
+            dt.Rows.Add(values23);
+            dt.Rows.Add(values24);
+            dt.Rows.Add(values25);
             return true;
         }
 
@@ -29,6 +94,71 @@ namespace Controls
         /// <returns>dt不为空，返回true，否则返回false</returns>
         public static bool GetAllByDepart(ref DataTable dt, string depart, ref string exception)
         {
+            dt.Columns.Add("ID");
+            dt.Columns.Add("Date");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Sex");
+            dt.Columns.Add("Depart");
+            dt.Columns.Add("Job");
+            dt.Columns.Add("IDNo");
+            dt.Columns.Add("Birthday");
+            dt.Columns.Add("Fund");
+            dt.Columns.Add("Character");
+            dt.Columns.Add("Company");
+            dt.Columns.Add("StartTime");
+            dt.Columns.Add("StopTime");
+            dt.Columns.Add("Status");
+            dt.Columns.Add("Comment");
+            string[] values1 = new string[] { "1", "2010-3-3", "高1", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values2 = new string[] { "2", "2010-3-3", "高2", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values3 = new string[] { "3", "2010-3-3", "高3", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values4 = new string[] { "4", "2010-3-3", "高4", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values5 = new string[] { "123456", "2010-3-3", "高5", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values6 = new string[] { "123456", "2010-3-3", "高6", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values7 = new string[] { "123456", "2010-3-3", "高7", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values8 = new string[] { "123456", "2010-3-3", "高8", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values9 = new string[] { "123456", "2010-3-3", "高9", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values10 = new string[] { "123456", "2010-3-3", "高10", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "test for comment" };
+            string[] values11 = new string[] { "123456", "2010-3-3", "高11", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values12 = new string[] { "123456", "2010-3-3", "高12", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "0", "" };
+            string[] values13 = new string[] { "123456", "2010-3-3", "高13", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values14 = new string[] { "123456", "2010-3-3", "高14", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "test for comment" };
+            string[] values15 = new string[] { "123456", "2010-3-3", "高15", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values16 = new string[] { "123456", "2010-3-3", "高16", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values17 = new string[] { "123456", "2010-3-3", "高17", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values18 = new string[] { "123456", "2010-3-3", "高18", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values19 = new string[] { "123456", "2010-3-3", "高19", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "1", "" };
+            string[] values20 = new string[] { "123456", "2010-3-3", "高20", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "2", "" };
+            string[] values21 = new string[] { "123456", "2010-3-3", "高21", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "2", "" };
+            string[] values22 = new string[] { "123456", "2010-3-3", "高22", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "3", "" };
+            string[] values23 = new string[] { "123456", "2010-3-3", "高23", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "3", "" };
+            string[] values24 = new string[] { "123456", "2010-3-3", "高24", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "4", "" };
+            string[] values25 = new string[] { "123456", "2010-3-3", "高25", "男", "医学院", "饲养员", "1234556789012", "1989-1-2", "学校", "劳务", "东林", "2013-7-10", "2013-8-10", "4", "" };
+            dt.Rows.Add(values1);
+            dt.Rows.Add(values2);
+            dt.Rows.Add(values3);
+            dt.Rows.Add(values4);
+            dt.Rows.Add(values5);
+            dt.Rows.Add(values6);
+            dt.Rows.Add(values7);
+            dt.Rows.Add(values8);
+            dt.Rows.Add(values9);
+            dt.Rows.Add(values10);
+            dt.Rows.Add(values11);
+            dt.Rows.Add(values12);
+            dt.Rows.Add(values13);
+            dt.Rows.Add(values14);
+            dt.Rows.Add(values15);
+            dt.Rows.Add(values16);
+            dt.Rows.Add(values17);
+            dt.Rows.Add(values18);
+            dt.Rows.Add(values19);
+            dt.Rows.Add(values20);
+            dt.Rows.Add(values21);
+            dt.Rows.Add(values22);
+            dt.Rows.Add(values23);
+            dt.Rows.Add(values24);
+            dt.Rows.Add(values25);
             return true;
         }
 
@@ -56,7 +186,7 @@ namespace Controls
         }
 
         /// <summary>
-        /// 根据被考评人ID删除考评人名单，删除成功返回true，否则返回false
+        /// 根据被考评人ID从备选考评人中删除考评人，删除成功返回true，否则返回false
         /// </summary>
         /// <param name="IDs">被考评人ID列表</param>
         /// <param name="exception"></param>
@@ -67,7 +197,7 @@ namespace Controls
         }
 
         /// <summary>
-        /// 删除所有考评人名单，删除成功返回true，否则返回false
+        /// 删除备选考评人名单中所有考评人名单，删除成功返回true，否则返回false
         /// </summary>
         /// <param name="exception"></param>
         /// <returns></returns>
@@ -96,11 +226,29 @@ namespace Controls
         /// <returns></returns>
         public static bool GetSelectableEvaluatorByDepart(ref DataTable table, string depart, ref string exception)
         {
+            table.Columns.Add("ID");
+            table.Columns.Add("Name");
+            table.Columns.Add("Sex");
+            table.Columns.Add("Company");
+            table.Columns.Add("Telephone");
+            table.Columns.Add("Relation");
+            string[] values1 = new string[] { "1234", "高1", "男", "Intel", "12345", "领导" };
+            string[] values2 = new string[] { "5678", "高2", "女", "AMD", "2345", "同事" };
+            string[] values3 = new string[] { "2345", "高3", "男", "EMC", "1413235", "同事" };
+            string[] values4 = new string[] { "6789", "高4", "男", "Lenovo", "543234", "下属" };
+            string[] values5 = new string[] { "1212", "高5", "男", "Apple", "515321432", "服务对象" };
+            string[] values6 = new string[] { "3213", "高6", "男", "IBM", "25132513", "下属" };
+            table.Rows.Add(values1);
+            table.Rows.Add(values2);
+            table.Rows.Add(values3);
+            table.Rows.Add(values4);
+            table.Rows.Add(values5);
+            table.Rows.Add(values6);
             return true;
         }
 
         /// <summary>
-        /// 提交考评人名单。提交成功返回true，否则返回false
+        /// 提交考评人名单（如果已存在则更新）。提交成功返回true，否则返回false
         /// </summary>
         /// <param name="evaluated">被考评人ID</param>
         /// <param name="evaluatorRelationDic">考评人的ID和关系字典</param>
@@ -120,11 +268,29 @@ namespace Controls
         /// <returns></returns>
         public static bool GetEvaluator(ref DataTable table, string evaluated, ref string exception)
         {
+            table.Columns.Add("ID");
+            table.Columns.Add("Name");
+            table.Columns.Add("Sex");
+            table.Columns.Add("Company");
+            table.Columns.Add("Telephone");
+            table.Columns.Add("Relation");
+            string[] values1 = new string[] { "1234", "高1", "男", "Intel", "12345", "领导" };
+            string[] values2 = new string[] { "5678", "高2", "女", "AMD", "2345", "同事" };
+            string[] values3 = new string[] { "2345", "高3", "男", "EMC", "1413235", "同事" };
+            string[] values4 = new string[] { "6789", "高4", "男", "Lenovo", "543234", "下属" };
+            string[] values5 = new string[] { "1212", "高5", "男", "Apple", "515321432", "服务对象" };
+            string[] values6 = new string[] { "3213", "高6", "男", "IBM", "25132513", "下属" };
+            table.Rows.Add(values1);
+            table.Rows.Add(values2);
+            table.Rows.Add(values3);
+            table.Rows.Add(values4);
+            table.Rows.Add(values5);
+            table.Rows.Add(values6);
             return true;
         }
 
         /// <summary>
-        /// 设置考评人名单，成功返回true，否则返回false
+        /// 设置考评人名单（如果已存在，则更新），成功返回true，否则返回false
         /// </summary>
         /// <param name="evaluated">被考评人ID</param>
         /// <param name="evaluatorList">考评人ID列表</param>
