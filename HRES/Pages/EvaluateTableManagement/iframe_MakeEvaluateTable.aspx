5eb351c7177b598561ec99b7a23afb2d08b74815
cntimothy <cntimothy1990@gmail.com>
2013-07-25 11:22:23 +0800

    ENH:add framework of iframe_make_evaluate_table page

---
+++ HRES/Pages/EvaluateTableManagement/iframe_MakeEvaluateTable.aspx
@@ -0,0 +1,219 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="iframe_MakeEvaluateTable.aspx.cs"
+    Inherits="HRES.Pages.EvaluateTableManagement.iframe_MakeEvaluateTable" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <x:PageManager ID="PageManager1" runat="server" AutoSizePanelID="Panel1" />
+    <x:Panel ID="Panel1" runat="server" BodyPadding="0px" ShowBorder="true" ShowHeader="true"
+        Title="考核表" Layout="Fit" AutoScroll="true">
+        <Items>
+            <x:Panel ID="Panel2" runat="server" BodyPadding="0px" ShowBorder="false" ShowHeader="false"
+                Title="Panel2" AutoScroll="true" AutoHeight="true">
+                <Items>
+                    <x:Toolbar ID="Toolbar1" runat="server" CssStyle="width:99.7%">
+                        <Items>
+                            <x:Button ID="关闭" runat="server" Text="Button">
+                            </x:Button>
+                            <x:ToolbarSeparator ID="ToolbarSeparator1" runat="server">
+                            </x:ToolbarSeparator>
+                            <x:Button ID="保存" runat="server" Text="Button">
+                            </x:Button>
+                            <x:ToolbarSeparator ID="ToolbarSeparator2" runat="server">
+                            </x:ToolbarSeparator>
+                            <x:Button ID="提交" runat="server" Text="Button">
+                            </x:Button>
+                            <x:ToolbarFill ID="ToolbarFill1" runat="server">
+                            </x:ToolbarFill>
+                            <x:Label ID="Label1" runat="server" Label="Label" Text="审核意见">
+                            </x:Label>
+                            <x:Label ID="Label2" runat="server" Label="Label" Text="">
+                            </x:Label>
+                        </Items>
+                    </x:Toolbar>
+                    <x:Panel ID="Panel3" runat="server" BodyPadding="5px" ShowBorder="true" ShowHeader="true"
+                        Title="关键岗位职责指标">
+                        <Items>
+                            <x:SimpleForm ID="SimpleForm1" runat="server" BodyPadding="5px" Title="1)" Collapsed="false" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm2" runat="server" BodyPadding="5px" Title="2)" Collapsed="false" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm3" runat="server" BodyPadding="5px" Title="3)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm4" runat="server" BodyPadding="5px" Title="4)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm5" runat="server" BodyPadding="5px" Title="5)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                        </Items>
+                    </x:Panel>
+                    <x:Panel ID="Panel4" runat="server" BodyPadding="5px" ShowBorder="true" ShowHeader="true"
+                        Title="关键岗位胜任能力指标">
+                        <Items>
+                            <x:SimpleForm ID="SimpleForm6" runat="server" BodyPadding="5px" Title="1)" Collapsed="false" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm7" runat="server" BodyPadding="5px" Title="2)" Collapsed="false" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm8" runat="server" BodyPadding="5px" Title="3)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm9" runat="server" BodyPadding="5px" Title="4)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm10" runat="server" BodyPadding="5px" Title="5)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                        </Items>
+                    </x:Panel>
+                    <x:Panel ID="Panel5" runat="server" BodyPadding="5px" ShowBorder="true" ShowHeader="true"
+                        Title="关键工作态度指标">
+                        <Items>
+                            <x:SimpleForm ID="SimpleForm11" runat="server" BodyPadding="5px" Title="1)" Collapsed="false" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm12" runat="server" BodyPadding="5px" Title="2)" Collapsed="false" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm13" runat="server" BodyPadding="5px" Title="3)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm14" runat="server" BodyPadding="5px" Title="4)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm15" runat="server" BodyPadding="5px" Title="5)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                        </Items>
+                    </x:Panel>
+                    <x:Panel ID="Panel6" runat="server" BodyPadding="5px" ShowBorder="true" ShowHeader="true"
+                        Title="岗位职责指标">
+                        <Items>
+                            <x:SimpleForm ID="SimpleForm16" runat="server" BodyPadding="5px" Title="1)" Collapsed="false" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm17" runat="server" BodyPadding="5px" Title="2)" Collapsed="false" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm18" runat="server" BodyPadding="5px" Title="3)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm19" runat="server" BodyPadding="5px" Title="4)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm20" runat="server" BodyPadding="5px" Title="5)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                        </Items>
+                    </x:Panel>
+                    <x:Panel ID="Panel7" runat="server" BodyPadding="5px" ShowBorder="true" ShowHeader="true"
+                        Title="岗位胜任能力指标">
+                        <Items>
+                            <x:SimpleForm ID="SimpleForm21" runat="server" BodyPadding="5px" Title="1)" Collapsed="false" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm22" runat="server" BodyPadding="5px" Title="2)" Collapsed="false" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm23" runat="server" BodyPadding="5px" Title="3)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm24" runat="server" BodyPadding="5px" Title="4)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm25" runat="server" BodyPadding="5px" Title="5)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                        </Items>
+                    </x:Panel>
+                    <x:Panel ID="Panel8" runat="server" BodyPadding="5px" ShowBorder="true" ShowHeader="true"
+                        Title="工作态度指标">
+                        <Items>
+                            <x:SimpleForm ID="SimpleForm26" runat="server" BodyPadding="5px" Title="1)" Collapsed="false" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm27" runat="server" BodyPadding="5px" Title="2)" Collapsed="false" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm28" runat="server" BodyPadding="5px" Title="3)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm29" runat="server" BodyPadding="5px" Title="4)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm30" runat="server" BodyPadding="5px" Title="5)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                        </Items>
+                    </x:Panel>
+                    <x:Panel ID="Panel9" runat="server" BodyPadding="5px" ShowBorder="true" ShowHeader="true"
+                        Title="否决指标">
+                        <Items>
+                            <x:SimpleForm ID="SimpleForm31" runat="server" BodyPadding="5px" Title="1)" Collapsed="false" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm32" runat="server" BodyPadding="5px" Title="2)" Collapsed="false" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm33" runat="server" BodyPadding="5px" Title="3)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm34" runat="server" BodyPadding="5px" Title="4)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                            <x:SimpleForm ID="SimpleForm35" runat="server" BodyPadding="5px" Title="5)" Collapsed="true" EnableCollapse="true">
+                                <Items>
+                                </Items>
+                            </x:SimpleForm>
+                        </Items>
+                    </x:Panel>
+                </Items>
+            </x:Panel>
+        </Items>
+    </x:Panel>
+    </form>
+</body>
+</html>
---
+++ HRES/Pages/EvaluateTableManagement/iframe_MakeEvaluateTable.aspx.designer.cs
@@ -0,0 +1,510 @@
+﻿//------------------------------------------------------------------------------
+// <自动生成>
+//     此代码由工具生成。
+//
+//     对此文件的更改可能会导致不正确的行为，并且如果
+//     重新生成代码，这些更改将会丢失。 
+// </自动生成>
+//------------------------------------------------------------------------------
+
+namespace HRES.Pages.EvaluateTableManagement {
+    
+    
+    public partial class iframe_MakeEvaluateTable {
+        
+        /// <summary>
+        /// form1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// PageManager1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.PageManager PageManager1;
+        
+        /// <summary>
+        /// Panel1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel1;
+        
+        /// <summary>
+        /// Panel2 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel2;
+        
+        /// <summary>
+        /// Toolbar1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Toolbar Toolbar1;
+        
+        /// <summary>
+        /// 关闭 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Button 关闭;
+        
+        /// <summary>
+        /// ToolbarSeparator1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.ToolbarSeparator ToolbarSeparator1;
+        
+        /// <summary>
+        /// 保存 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Button 保存;
+        
+        /// <summary>
+        /// ToolbarSeparator2 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.ToolbarSeparator ToolbarSeparator2;
+        
+        /// <summary>
+        /// 提交 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Button 提交;
+        
+        /// <summary>
+        /// ToolbarFill1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.ToolbarFill ToolbarFill1;
+        
+        /// <summary>
+        /// Label1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Label Label1;
+        
+        /// <summary>
+        /// Label2 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Label Label2;
+        
+        /// <summary>
+        /// Panel3 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel3;
+        
+        /// <summary>
+        /// SimpleForm1 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm1;
+        
+        /// <summary>
+        /// SimpleForm2 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm2;
+        
+        /// <summary>
+        /// SimpleForm3 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm3;
+        
+        /// <summary>
+        /// SimpleForm4 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm4;
+        
+        /// <summary>
+        /// SimpleForm5 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm5;
+        
+        /// <summary>
+        /// Panel4 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel4;
+        
+        /// <summary>
+        /// SimpleForm6 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm6;
+        
+        /// <summary>
+        /// SimpleForm7 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm7;
+        
+        /// <summary>
+        /// SimpleForm8 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm8;
+        
+        /// <summary>
+        /// SimpleForm9 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm9;
+        
+        /// <summary>
+        /// SimpleForm10 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm10;
+        
+        /// <summary>
+        /// Panel5 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel5;
+        
+        /// <summary>
+        /// SimpleForm11 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm11;
+        
+        /// <summary>
+        /// SimpleForm12 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm12;
+        
+        /// <summary>
+        /// SimpleForm13 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm13;
+        
+        /// <summary>
+        /// SimpleForm14 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm14;
+        
+        /// <summary>
+        /// SimpleForm15 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm15;
+        
+        /// <summary>
+        /// Panel6 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel6;
+        
+        /// <summary>
+        /// SimpleForm16 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm16;
+        
+        /// <summary>
+        /// SimpleForm17 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm17;
+        
+        /// <summary>
+        /// SimpleForm18 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm18;
+        
+        /// <summary>
+        /// SimpleForm19 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm19;
+        
+        /// <summary>
+        /// SimpleForm20 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm20;
+        
+        /// <summary>
+        /// Panel7 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel7;
+        
+        /// <summary>
+        /// SimpleForm21 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm21;
+        
+        /// <summary>
+        /// SimpleForm22 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm22;
+        
+        /// <summary>
+        /// SimpleForm23 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm23;
+        
+        /// <summary>
+        /// SimpleForm24 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm24;
+        
+        /// <summary>
+        /// SimpleForm25 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm25;
+        
+        /// <summary>
+        /// Panel8 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel8;
+        
+        /// <summary>
+        /// SimpleForm26 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm26;
+        
+        /// <summary>
+        /// SimpleForm27 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm27;
+        
+        /// <summary>
+        /// SimpleForm28 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm28;
+        
+        /// <summary>
+        /// SimpleForm29 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm29;
+        
+        /// <summary>
+        /// SimpleForm30 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm30;
+        
+        /// <summary>
+        /// Panel9 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.Panel Panel9;
+        
+        /// <summary>
+        /// SimpleForm31 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm31;
+        
+        /// <summary>
+        /// SimpleForm32 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm32;
+        
+        /// <summary>
+        /// SimpleForm33 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm33;
+        
+        /// <summary>
+        /// SimpleForm34 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm34;
+        
+        /// <summary>
+        /// SimpleForm35 控件。
+        /// </summary>
+        /// <remarks>
+        /// 自动生成的字段。
+        /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
+        /// </remarks>
+        protected global::FineUI.SimpleForm SimpleForm35;
+    }
+}
