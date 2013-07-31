a3233b98667a094abb1df48681798918959a33f7
cntimothy <cntimothy1990@gmail.com>
2013-07-31 17:31:00 +0800

    ENH:finish work of stop_evaluation

--- HRES/Pages/EvaluationManagement/StopEvaluation.aspx.cs
+++ HRES/Pages/EvaluationManagement/StopEvaluation.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using FineUI;
+using System.Data;
+using Controls;
 
 namespace HRES.Pages.EvaluationManagement
 {
@@ -11,7 +14,95 @@ namespace HRES.Pages.EvaluationManagement
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                bindEvaluatedToGrid();
+            }
         }
+
+        #region Event
+        protected void Grid1_PageIndexChange(object sender, FineUI.GridPageEventArgs e)
+        {
+            Grid1.PageIndex = e.NewPageIndex;
+        }
+
+        protected void Grid1_RowClick(object sender, FineUI.GridRowClickEventArgs e)
+        {
+            object[] keys = Grid1.DataKeys[e.RowIndex];
+            SetSimpleForm(keys);
+        }
+
+        protected void Button_Refresh_Click(object sender, EventArgs e)
+        {
+            bindEvaluatedToGrid();
+        }
+
+        protected void Button_Start_Click(object sender, EventArgs e)
+        {
+            string exception = "";
+            if (EvaluationManagementCtrl.StartEvaluation(ref exception))
+            {
+                Alert.ShowInTop("设置成功！", MessageBoxIcon.Information);
+            }
+            else
+            {
+                Alert.ShowInTop("设置失败！\n原因：" + exception, MessageBoxIcon.Error);
+            }
+        }
+
+        protected void DropDownList1_SelectedChanged(object sender, EventArgs e)
+        {
+            bindEvaluatedToGrid();
+        }
+
+        protected void Grid1_RowCommand(object sender, FineUI.GridCommandEventArgs e)
+        {
+            string exception = "";
+            if (e.CommandName == "Stop")
+            {
+                object[] keys = Grid1.DataKeys[e.RowIndex];
+                string id = (string)keys[0];
+                if (EvaluationManagementCtrl.StopEvaluation(id, ref exception))
+                {
+                    Alert.ShowInTop("设置成功！", MessageBoxIcon.Information);
+                }
+                else
+                {
+                    Alert.ShowInTop("设置失败！\n原因：" + exception, MessageBoxIcon.Error);
+                }
+                bindEvaluatedToGrid();
+            }
+        }
+        #endregion
+
+        #region Private Method
+        private void bindEvaluatedToGrid()
+        {
+            string exception = "";
+            DataTable table = new DataTable();
+            string depart = Session["Depart"].ToString();
+            if (EvaluationManagementCtrl.GetAllByDepart(ref table, depart, ref exception))
+            {
+                Grid1.DataSource = table;
+                Grid1.DataBind();
+            }
+        }
+        private void SetSimpleForm(object[] keys)
+        {
+            LabID.Text = (string)keys[0];
+            LabDate.Text = (string)keys[1];
+            LabName.Text = (string)keys[2];
+            LabSex.Text = (string)keys[3];
+            LabDepart.Text = (string)keys[4];
+            LabJob.Text = (string)keys[5];
+            LabIDNo.Text = (string)keys[6];
+            LabBirthday.Text = (string)keys[7];
+            LabFund.Text = (string)keys[8];
+            LabCharacter.Text = (string)keys[9];
+            LabCompany.Text = (string)keys[10];
+            LabStartTime.Text = (string)keys[11];
+            LabStopTime.Text = (string)keys[12];
+        }
+        #endregion
     }
 }
