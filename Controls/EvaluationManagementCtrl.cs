af8743822b78ea8981bd08ebd4b208c6d1326205
cntimothy <cntimothy1990@gmail.com>
2013-09-04 19:38:20 +0800

    ENH:change messageplatform to first class menu and add send message to seconds

--- Controls/EvaluationManagementCtrl.cs
+++ Controls/EvaluationManagementCtrl.cs
@@ -301,38 +301,6 @@ namespace Controls
         }
 
         /// <summary>
-        /// 给所有考评人发送短信，发送成功返回true，否则返回false
-        /// </summary>
-        /// <param name="exception"></param>
-        /// <returns></returns>
-        public static bool SendMessageToAll(string message, ref string exception)
-        {
-            return true;
-        }
-
-        /// <summary>
-        /// 分部门给考评人发送短信，发送成功返回true，否则返回false
-        /// </summary>
-        /// <param name="depart"></param>
-        /// <param name="exception"></param>
-        /// <returns></returns>
-        public static bool SendMessageByDepart(string message, string depart, ref string exception)
-        {
-            return true;
-        }
-
-        /// <summary>
-        /// 给指定的考评人发送短信，发送成功返回true，否则返回false
-        /// </summary>
-        /// <param name="IDs"></param>
-        /// <param name="exception"></param>
-        /// <returns></returns>
-        public static bool SendMessage(List<string> IDs, string message, ref string exception)
-        {
-            return true;
-        }
-
-        /// <summary>
         /// 返回今年的被考评人信息（ID, Name, Sex, Company(派遣公司、用人单位), Depart(用工单位), LaborDepart(用工部门), PostName(岗位名称), PostType(岗位类别), Fund(经费来源), Character(派遣性质), StartTime, StopTime, Status(按EvaluationStatus传值))table不为空返回true，否则返回false
         /// </summary>
         /// <param name="table">被考评人信息</param>
@@ -512,7 +480,7 @@ namespace Controls
             result.Is360 = true;
             result.PostName = "岗位名称";
             result.LaborDep = "工作部门";
-            result.LaborUnit = "工作单位";
+            result.LaborUnit = "用工单位";
             result.StartTime = "开始时间";
             result.StopTime = "结束时间";
             result.KeyScore = new string[] { "34", "87", "67", "48", "87" };
@@ -584,111 +552,111 @@ namespace Controls
             return true;
         }
 
-        /// <summary>
-        /// 查询所有考评人信息（ID, Name, Sex, Depart(部门), Company(单位), Telephone），非空返回true，否则返回false。
-        /// </summary>
-        /// <param name="table"></param>
-        /// <param name="exception"></param>
-        /// <returns></returns>
-        public static bool GetEvaluators(ref DataTable table, ref string exception)
-        {
-            table.Columns.Add("ID");
-            table.Columns.Add("Name");
-            table.Columns.Add("Sex");
-            table.Columns.Add("Depart");
-            table.Columns.Add("Company");
-            table.Columns.Add("Telephone");
-            string[] values1 = new string[] { "1", "高1", "男", "Intel", "医学院", "12345" };
-            string[] values2 = new string[] { "2", "高2", "女", "AMD", "医学院", "2345" };
-            string[] values3 = new string[] { "3", "高3", "男", "EMC", "医学院", "1413235" };
-            string[] values4 = new string[] { "4", "高4", "男", "Lenovo", "医学院", "543234" };
-            string[] values5 = new string[] { "5", "高5", "男", "Apple", "医学院", "515321432" };
-            string[] values6 = new string[] { "6", "高6", "男", "IBM", "医学院", "25132513" };
-            string[] values7 = new string[] { "7", "高1", "男", "Intel", "医学院", "12345" };
-            string[] values8 = new string[] { "8", "高2", "女", "AMD", "医学院", "2345" };
-            string[] values9 = new string[] { "9", "高3", "男", "EMC", "医学院", "1413235" };
-            string[] values10 = new string[] { "0", "高4", "男", "Lenovo", "外国语学院", "543234" };
-            string[] values11 = new string[] { "11", "高5", "男", "Apple", "外国语学院", "515321432" };
-            string[] values12 = new string[] { "22", "高6", "男", "IBM", "外国语学院", "25132513" };
-            string[] values13 = new string[] { "33", "高1", "男", "Intel", "外国语学院", "12345" };
-            string[] values14 = new string[] { "44", "高2", "女", "AMD", "外国语学院", "2345" };
-            string[] values15 = new string[] { "55", "高3", "男", "EMC", "医学院", "1413235" };
-            string[] values16 = new string[] { "66", "高4", "男", "Lenovo", "医学院", "543234" };
-            string[] values17 = new string[] { "77", "高5", "男", "Apple", "医学院", "515321432" };
-            string[] values18 = new string[] { "88", "高6", "男", "IBM", "医学院", "25132513" };
-            table.Rows.Add(values1);
-            table.Rows.Add(values2);
-            table.Rows.Add(values3);
-            table.Rows.Add(values4);
-            table.Rows.Add(values5);
-            table.Rows.Add(values6);
-            table.Rows.Add(values7);
-            table.Rows.Add(values8);
-            table.Rows.Add(values9);
-            table.Rows.Add(values10);
-            table.Rows.Add(values11);
-            table.Rows.Add(values12);
-            table.Rows.Add(values13);
-            table.Rows.Add(values14);
-            table.Rows.Add(values15);
-            table.Rows.Add(values16);
-            table.Rows.Add(values17);
-            table.Rows.Add(values18);
-            return true;
-        }
+        ///// <summary>
+        ///// 查询所有考评人信息（ID, Name, Sex, Depart(部门), Company(单位), Telephone），非空返回true，否则返回false。
+        ///// </summary>
+        ///// <param name="table"></param>
+        ///// <param name="exception"></param>
+        ///// <returns></returns>
+        //public static bool GetEvaluators(ref DataTable table, ref string exception)
+        //{
+        //    table.Columns.Add("ID");
+        //    table.Columns.Add("Name");
+        //    table.Columns.Add("Sex");
+        //    table.Columns.Add("Depart");
+        //    table.Columns.Add("Company");
+        //    table.Columns.Add("Telephone");
+        //    string[] values1 = new string[] { "1", "高1", "男", "Intel", "医学院", "12345" };
+        //    string[] values2 = new string[] { "2", "高2", "女", "AMD", "医学院", "2345" };
+        //    string[] values3 = new string[] { "3", "高3", "男", "EMC", "医学院", "1413235" };
+        //    string[] values4 = new string[] { "4", "高4", "男", "Lenovo", "医学院", "543234" };
+        //    string[] values5 = new string[] { "5", "高5", "男", "Apple", "医学院", "515321432" };
+        //    string[] values6 = new string[] { "6", "高6", "男", "IBM", "医学院", "25132513" };
+        //    string[] values7 = new string[] { "7", "高1", "男", "Intel", "医学院", "12345" };
+        //    string[] values8 = new string[] { "8", "高2", "女", "AMD", "医学院", "2345" };
+        //    string[] values9 = new string[] { "9", "高3", "男", "EMC", "医学院", "1413235" };
+        //    string[] values10 = new string[] { "0", "高4", "男", "Lenovo", "外国语学院", "543234" };
+        //    string[] values11 = new string[] { "11", "高5", "男", "Apple", "外国语学院", "515321432" };
+        //    string[] values12 = new string[] { "22", "高6", "男", "IBM", "外国语学院", "25132513" };
+        //    string[] values13 = new string[] { "33", "高1", "男", "Intel", "外国语学院", "12345" };
+        //    string[] values14 = new string[] { "44", "高2", "女", "AMD", "外国语学院", "2345" };
+        //    string[] values15 = new string[] { "55", "高3", "男", "EMC", "医学院", "1413235" };
+        //    string[] values16 = new string[] { "66", "高4", "男", "Lenovo", "医学院", "543234" };
+        //    string[] values17 = new string[] { "77", "高5", "男", "Apple", "医学院", "515321432" };
+        //    string[] values18 = new string[] { "88", "高6", "男", "IBM", "医学院", "25132513" };
+        //    table.Rows.Add(values1);
+        //    table.Rows.Add(values2);
+        //    table.Rows.Add(values3);
+        //    table.Rows.Add(values4);
+        //    table.Rows.Add(values5);
+        //    table.Rows.Add(values6);
+        //    table.Rows.Add(values7);
+        //    table.Rows.Add(values8);
+        //    table.Rows.Add(values9);
+        //    table.Rows.Add(values10);
+        //    table.Rows.Add(values11);
+        //    table.Rows.Add(values12);
+        //    table.Rows.Add(values13);
+        //    table.Rows.Add(values14);
+        //    table.Rows.Add(values15);
+        //    table.Rows.Add(values16);
+        //    table.Rows.Add(values17);
+        //    table.Rows.Add(values18);
+        //    return true;
+        //}
 
-        /// <summary>
-        /// 根据部门查询所有考评人信息（ID, Name, Sex, Depart(部门), Company(单位), Telephone），非空返回true，否则返回false。
-        /// </summary>
-        /// <param name="table"></param>
-        /// <param name="exception"></param>
-        /// <returns></returns>
-        public static bool GetEvaluatorsByDepart(ref DataTable table, string depart, ref string exception)
-        {
-            table.Columns.Add("ID");
-            table.Columns.Add("Name");
-            table.Columns.Add("Sex");
-            table.Columns.Add("Depart");
-            table.Columns.Add("Company");
-            table.Columns.Add("Telephone");
-            string[] values1 = new string[] { "1", "高1", "男", "Intel", "医学院", "12345" };
-            string[] values2 = new string[] { "2", "高2", "女", "AMD", "医学院", "2345" };
-            string[] values3 = new string[] { "3", "高3", "男", "EMC", "医学院", "1413235" };
-            string[] values4 = new string[] { "4", "高4", "男", "Lenovo", "医学院", "543234" };
-            string[] values5 = new string[] { "5", "高5", "男", "Apple", "医学院", "515321432" };
-            string[] values6 = new string[] { "6", "高6", "男", "IBM", "医学院", "25132513" };
-            string[] values7 = new string[] { "7", "高1", "男", "Intel", "医学院", "12345" };
-            string[] values8 = new string[] { "8", "高2", "女", "AMD", "医学院", "2345" };
-            string[] values9 = new string[] { "9", "高3", "男", "EMC", "医学院", "1413235" };
-            string[] values10 = new string[] { "0", "高4", "男", "Lenovo", "医学院", "543234" };
-            string[] values11 = new string[] { "11", "高5", "男", "Apple", "医学院", "515321432" };
-            string[] values12 = new string[] { "22", "高6", "男", "IBM", "医学院", "25132513" };
-            string[] values13 = new string[] { "33", "高1", "男", "Intel", "医学院", "12345" };
-            string[] values14 = new string[] { "44", "高2", "女", "AMD", "医学院", "2345" };
-            string[] values15 = new string[] { "55", "高3", "男", "EMC", "医学院", "1413235" };
-            string[] values16 = new string[] { "66", "高4", "男", "Lenovo", "医学院", "543234" };
-            string[] values17 = new string[] { "77", "高5", "男", "Apple", "医学院", "515321432" };
-            string[] values18 = new string[] { "88", "高6", "男", "IBM", "医学院", "25132513" };
-            table.Rows.Add(values1);
-            table.Rows.Add(values2);
-            table.Rows.Add(values3);
-            table.Rows.Add(values4);
-            table.Rows.Add(values5);
-            table.Rows.Add(values6);
-            table.Rows.Add(values7);
-            table.Rows.Add(values8);
-            table.Rows.Add(values9);
-            table.Rows.Add(values10);
-            table.Rows.Add(values11);
-            table.Rows.Add(values12);
-            table.Rows.Add(values13);
-            table.Rows.Add(values14);
-            table.Rows.Add(values15);
-            table.Rows.Add(values16);
-            table.Rows.Add(values17);
-            table.Rows.Add(values18);
-            return true;
-        }
+        ///// <summary>
+        ///// 根据部门查询所有考评人信息（ID, Name, Sex, Depart(部门), Company(单位), Telephone），非空返回true，否则返回false。
+        ///// </summary>
+        ///// <param name="table"></param>
+        ///// <param name="exception"></param>
+        ///// <returns></returns>
+        //public static bool GetEvaluatorsByDepart(ref DataTable table, string depart, ref string exception)
+        //{
+        //    table.Columns.Add("ID");
+        //    table.Columns.Add("Name");
+        //    table.Columns.Add("Sex");
+        //    table.Columns.Add("Depart");
+        //    table.Columns.Add("Company");
+        //    table.Columns.Add("Telephone");
+        //    string[] values1 = new string[] { "1", "高1", "男", "Intel", "医学院", "12345" };
+        //    string[] values2 = new string[] { "2", "高2", "女", "AMD", "医学院", "2345" };
+        //    string[] values3 = new string[] { "3", "高3", "男", "EMC", "医学院", "1413235" };
+        //    string[] values4 = new string[] { "4", "高4", "男", "Lenovo", "医学院", "543234" };
+        //    string[] values5 = new string[] { "5", "高5", "男", "Apple", "医学院", "515321432" };
+        //    string[] values6 = new string[] { "6", "高6", "男", "IBM", "医学院", "25132513" };
+        //    string[] values7 = new string[] { "7", "高1", "男", "Intel", "医学院", "12345" };
+        //    string[] values8 = new string[] { "8", "高2", "女", "AMD", "医学院", "2345" };
+        //    string[] values9 = new string[] { "9", "高3", "男", "EMC", "医学院", "1413235" };
+        //    string[] values10 = new string[] { "0", "高4", "男", "Lenovo", "医学院", "543234" };
+        //    string[] values11 = new string[] { "11", "高5", "男", "Apple", "医学院", "515321432" };
+        //    string[] values12 = new string[] { "22", "高6", "男", "IBM", "医学院", "25132513" };
+        //    string[] values13 = new string[] { "33", "高1", "男", "Intel", "医学院", "12345" };
+        //    string[] values14 = new string[] { "44", "高2", "女", "AMD", "医学院", "2345" };
+        //    string[] values15 = new string[] { "55", "高3", "男", "EMC", "医学院", "1413235" };
+        //    string[] values16 = new string[] { "66", "高4", "男", "Lenovo", "医学院", "543234" };
+        //    string[] values17 = new string[] { "77", "高5", "男", "Apple", "医学院", "515321432" };
+        //    string[] values18 = new string[] { "88", "高6", "男", "IBM", "医学院", "25132513" };
+        //    table.Rows.Add(values1);
+        //    table.Rows.Add(values2);
+        //    table.Rows.Add(values3);
+        //    table.Rows.Add(values4);
+        //    table.Rows.Add(values5);
+        //    table.Rows.Add(values6);
+        //    table.Rows.Add(values7);
+        //    table.Rows.Add(values8);
+        //    table.Rows.Add(values9);
+        //    table.Rows.Add(values10);
+        //    table.Rows.Add(values11);
+        //    table.Rows.Add(values12);
+        //    table.Rows.Add(values13);
+        //    table.Rows.Add(values14);
+        //    table.Rows.Add(values15);
+        //    table.Rows.Add(values16);
+        //    table.Rows.Add(values17);
+        //    table.Rows.Add(values18);
+        //    return true;
+        //}
 
         /// <summary>
         /// 查询考核年份的列表
